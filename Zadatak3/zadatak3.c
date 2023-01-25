7af0d0337c13fea89710b4864968a27d4b0a80ec
MarkoTagi <markotagi@gmail.com>
2023-01-26 00:00:03 +0100

    Pravilno stampanje citavog stabla

--- Zadatak3/zadatak3.c
+++ Zadatak3/zadatak3.c
@@ -10,14 +10,18 @@
 #define MAX_LENGTH 1024
 
 int containsString(const char*, const char*);
-int findPattern(const char* filePath, const char* pattern);
+int findPattern(const char* filePath, const char* pattern, int indentCount);
 
 int main(int argc, char** argv) {
     if (argc != 3) {
         fprintf(stderr, "bad usage: %s\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    int filesFound = findPattern(argv[1], argv[2]);
+    int filesFound = findPattern(argv[1], argv[2], 0);
+    if (errno != 0) {
+        perror("Failed to find the files!\nReason");
+        exit(EXIT_FAILURE);
+    }
     printf("Files containing pattern [%s]: %d.\n", argv[2], filesFound);
     return 0;
 }
@@ -36,7 +40,7 @@ int containsString(const char* text, const char* pattern) {
     return 0;
 }
 
-int findPattern(const char* filePath, const char* pattern) {
+int findPattern(const char* filePath, const char* pattern, int indentCount) {
     int patternCount = 0;
     DIR* directoryPointer = opendir(filePath);
     if (directoryPointer == NULL) {
@@ -46,11 +50,16 @@ int findPattern(const char* filePath, const char* pattern) {
     struct dirent* directoryEntry = (struct dirent*)malloc(sizeof(struct dirent));
     errno = 0;
     while ((directoryEntry = readdir(directoryPointer)) != NULL) {
+        int notSelfOrParent = (strcmp(directoryEntry->d_name, ".") != 0) && (strcmp(directoryEntry->d_name, "..") != 0);
+        if (notSelfOrParent) {
+            for (int i = 0; i < indentCount; ++i) printf("\t");
+            printf("\\->");
+        }
         if (containsString(directoryEntry->d_name, pattern)) {
             ++patternCount;
             printf("%s [*]\n", directoryEntry->d_name);
         }
-        else if ((strcmp(directoryEntry->d_name, ".") != 0) && strcmp(directoryEntry->d_name, "..") != 0) printf("%s\n", directoryEntry->d_name);
+        else if (notSelfOrParent) printf("%s\n", directoryEntry->d_name);
         char newFilePath[MAX_LENGTH]; strcpy(newFilePath, filePath);
         strcat(newFilePath, "/"); strcat(newFilePath, directoryEntry->d_name);
         struct stat* fileStats = (struct stat*)malloc(sizeof(struct stat));
@@ -59,7 +68,10 @@ int findPattern(const char* filePath, const char* pattern) {
             perror("Failed to get file statistics!\nReason");
             exit(EXIT_FAILURE);
         }
-        if ((strcmp(directoryEntry->d_name, ".") != 0) && (strcmp(directoryEntry->d_name, "..") != 0) && S_ISDIR(fileStats->st_mode)) patternCount += findPattern(newFilePath, pattern);
+        if (notSelfOrParent && S_ISDIR(fileStats->st_mode)) {
+            patternCount += findPattern(newFilePath, pattern, ++indentCount);
+            --indentCount;
+        }
         free(fileStats);
     }
     free(directoryEntry);
